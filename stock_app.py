7e3ac1ad5d3efc0fe43a414785cd15bc9d1c5db8
rsanchezafi <75159687+rsanchezafi@users.noreply.github.com>
2021-05-27 12:26:35 +0200

    Update stock_app.py

--- stock_app.py
+++ stock_app.py
@@ -1,8 +1,5 @@
 import datetime
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import yfinance as yf # https://pypi.org/project/yfinance/
 from ta.volatility import BollingerBands
 from ta.trend import MACD
